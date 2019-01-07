a0e1593e5420a6ba11e986bd98314c511f81e5e2
Gabriel Mazzoco Guilherme <gmguilherme@stefanini.com>
2019-01-07 14:38:53 -0200

    Criacao classe User

--- okrapp - API/okrapp.API/Startup.cs
+++ okrapp - API/okrapp.API/Startup.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace okr.API
+namespace okrapp.API
 {
     public class Startup
     {
@@ -34,7 +35,13 @@ namespace okr.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
